15042a79eeb7d0936ef2f798bfb4d18c0b2acdfd
suinming <0223314338aa@gmail.com>
2023-06-20 11:29:02 +0800

    feat: add textwidth=72 to auto warp the line

--- nvim/user/options.lua
+++ nvim/user/options.lua
@@ -6,8 +6,9 @@ return {
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-    wrap = false, -- sets vim.opt.wrap
+    wrap = true, -- sets vim.opt.wrap
     showtabline = 0,
+    textwidth = 72,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
