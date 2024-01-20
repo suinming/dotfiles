5d7db5a65ef23a4ce1b181e1956a1874f4f39efd
suinming <0223314338aa@gmail.com>
2024-01-20 12:07:25 +0800

    feat: adjust gf cmd

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -26,3 +26,9 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- gf cmd
+vim.cmd("set path=.,src,@")
+vim.cmd("set suffixesadd=.js,.jsx,.vue")
+vim.cmd("set isfname+=@-@")
+vim.cmd("set includeexpr=substitute(v:fname,'^@/','src/','')")
